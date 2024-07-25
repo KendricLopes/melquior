caae854c127dbd893cefc9fa5038de24c6d7d728
KendricLopes <KendricLopes@users.noreply.github.com>
2024-07-25 14:03:56 +0000

    deploy: f839657ea166bdbac78853ba34095084dc81f749

---
+++ _nuxt/5CdywcYR.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./DhiVAeNn.js";const r={};function t(n,s){return c(),o("div",null," Projetos ")}const a=e(r,[["render",t]]);export{a as default};
---
+++ _nuxt/DxnBx3Jg.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as c}from"./DhiVAeNn.js";const e={};function n(r,a){return c(),t("div",null," Contato ")}const _=o(e,[["render",n]]);export{_ as default};
