f8f41f7b89f917f189052951dba41a6c01131014
Kendric Lopes <128600285+KendricLopes@users.noreply.github.com>
2024-07-25 11:05:46 -0300

    Update Navegacao.vue

--- components/Navegacao.vue
+++ components/Navegacao.vue
@@ -1,9 +1,9 @@
 <template>
     <nav>
         <ul>
-            <li><NuxtLink to="melquior/">Home</NuxtLink></li>
-            <li><NuxtLink to="melquior/projetos">Projetos</NuxtLink></li>
-            <li><NuxtLink to="melquior/contato">Contato</NuxtLink></li>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/projetos">Projetos</NuxtLink></li>
+            <li><NuxtLink to="/contato">Contato</NuxtLink></li>
         </ul>
     </nav>
 </template>
